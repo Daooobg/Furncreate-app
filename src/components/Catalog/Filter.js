2f9a884958e025db0971cd273be1c7335976183b
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-03-24 18:00:56 +0000

    Create filters for catalog

---
+++ src/components/Catalog/Filter.js
@@ -0,0 +1,84 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import { productsActions } from '../../store/products-slice';
+import { formatPrice, getUniqueValues } from '../../UI/helpers';
+import classes from './Filter.module.css';
+
+const Filter = () => {
+  const data = useSelector((state) => state.products);
+  const products = data.products;
+  const category = data.filters.category;
+  const color = data.filters.colors;
+  const categories = getUniqueValues(products, 'type');
+  const colors = getUniqueValues(products, 'color');
+  const { min_price, max_price, price } = data.filters;
+  const dispatch = useDispatch();
+  const updateFiltersHandler = (e) => {
+    let name = e.target.name;
+    let value = e.target.textContent;
+    if (name === 'price' || name === 'color') {
+      value = e.target.value;
+    }
+    dispatch(
+      productsActions.updateFiltersValue({
+        name,
+        value,
+      })
+    );
+    dispatch(productsActions.filter());
+  };
+
+  return (
+    <div className={classes.container}>
+      <div className="form-control">
+        <h4>category</h4>
+        <div>
+          {categories.map((c, index) => {
+            return (
+              <button
+                key={index}
+                onClick={updateFiltersHandler}
+                name="category"
+                type="button"
+                className={`${
+                  category === c.toLowerCase()
+                    ? `${classes.active} ${classes['nav-link']}`
+                    : `${classes['nav-link']}`
+                }`}
+              >
+                {c}
+              </button>
+            );
+          })}
+        </div>
+      </div>
+
+      <div className="form-control">
+        <h4>Colors</h4>
+        <select name="color" value={color} onChange={updateFiltersHandler}>
+          {colors.map((c, index) => {
+            return (
+              <option value={c} key={index}>
+                {c}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+      <div className="form-control">
+        <h5>price</h5>
+        <p className="price">{formatPrice(price)}</p>
+        <input
+          type="range"
+          name="price"
+          onChange={updateFiltersHandler}
+          min={min_price}
+          max={max_price}
+          value={price}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Filter;
