143c0efd337dc171a5d669070ba40491a1e60d3d
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-03-24 18:01:57 +0000

    Create furniture list for catalog

---
+++ src/components/Catalog/FurnitureList.js
@@ -0,0 +1,31 @@
+import { Link } from 'react-router-dom';
+
+import { useSelector } from 'react-redux';
+import classes from './FurnitureList.module.css';
+import { formatPrice } from '../../UI/helpers';
+
+const FurnitureList = () => {
+  const filtered_products = useSelector(
+    (state) => state.products.filteredProducts
+  );
+  console.log('filtered_products', filtered_products);
+  
+  return (
+    <div className={classes['container-grid']}>
+      {filtered_products.map((product) => {
+        return (
+          <Link to={product.slug} key={product._id} className={classes["image-container"]}>
+            <img className={classes.img} src={product.img} alt={product.name} />
+            <div className={classes.text}>
+              <h3>Product: {product.name}</h3>
+              <h5>Set: {product.shortDescription}</h5>
+              <h3>Price: {formatPrice(product.price)}</h3>
+            </div>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default FurnitureList;
---
+++ src/components/Catalog/Sort.js
@@ -0,0 +1,65 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import { productsActions } from '../../store/products-slice';
+import classes from './Sort.module.css';
+
+const Sort = ({ products }) => {
+  const dispatch = useDispatch();
+
+  const sort = useSelector((state) => state.products.sort);
+  const filteredText = useSelector((state) => state.products.filters.text);
+
+  const sortProductsHandler = (e) => {
+    dispatch(productsActions.sortAllProducts(e.target.value));
+  };
+
+  const filteredTextHandler = (e) => {
+    dispatch(
+      productsActions.updateFiltersValue({
+        name: e.target.name,
+        value: e.target.value,
+      })
+    );
+    dispatch(productsActions.filter());
+  };
+
+  return (
+    <div className={classes.container}>
+      <div>
+        <div className="form-control">
+          <input
+            type="text"
+            name="text"
+            placeholder="search"
+            className="search-input"
+            value={filteredText}
+            onChange={filteredTextHandler}
+          />
+        </div>
+      </div>
+      <hr className={classes.dashed} />
+      <h4>{products.filteredProducts.length} Products found</h4>
+      <hr className={classes.dashed} />
+
+      <form>
+        <label htmlFor="sort">sort by</label>
+        <select
+          name="sort"
+          id="sort"
+          className="sort-input"
+          value={sort}
+          onChange={sortProductsHandler}
+        >
+          <option value="empty">------</option>
+          <option value="price-lowest">price (lowest)</option>
+          <option value="price-highest">price (highest)</option>
+          <option value="name-a">name (a-z)</option>
+          <option value="name-z">name (z-a)</option>
+          <option value="new">date (newest)</option>
+        </select>
+      </form>
+    </div>
+  );
+};
+
+export default Sort;
