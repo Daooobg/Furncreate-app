101112694fffdf455eca2e2b31195dcce7b6b504
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-03-24 17:54:58 +0000

    Add catalog root

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,22 +1,25 @@
+import { Link } from 'react-router-dom';
 import classes from './Home.module.css';
 
 const Home = () => {
   return (
-      <div className={classes.banner}>
-        <h1>
-          Welcome to <span>Furncreate</span>
-        </h1>
-        <p>
-          We offer a wide selection of high-quality furniture to fit any style
-          and budget.
-        </p>
-        <button type="button" className={classes['btn-left']}>
-          Gallery
-        </button>
+    <div className={classes.banner}>
+      <h1>
+        Welcome to <span>Furncreate</span>
+      </h1>
+      <p>
+        We offer a wide selection of high-quality furniture to fit any style and
+        budget.
+      </p>
+      <button type="button" className={classes['btn-left']}>
+        Gallery
+      </button>
+      <Link to={'/catalog'}>
         <button type="button" className={classes['btn-right']}>
           Shop now!
         </button>
-      </div>
+      </Link>
+    </div>
   );
 };
 
