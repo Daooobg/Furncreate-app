0658b3cdefb045b38dd185ff7c30c40337f49c3c
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-04-03 14:28:14 +0100

    Style changes and add favicons

--- src/components/Catalog/FurnitureList.js
+++ src/components/Catalog/FurnitureList.js
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
-
 import { useSelector } from 'react-redux';
+import { BsBasket } from 'react-icons/bs';
+import { TbListDetails } from 'react-icons/tb';
+
 import classes from './FurnitureList.module.css';
 import { formatPrice } from '../../UI/helpers';
 
@@ -8,19 +10,20 @@ const FurnitureList = () => {
   const filtered_products = useSelector(
     (state) => state.products.filteredProducts
   );
-  
+
   return (
     <div className={classes['container-grid']}>
       {filtered_products.map((product) => {
         return (
-          <Link to={product.slug} key={product._id} className={classes["image-container"]}>
+          <div className={classes['image-container']}>
             <img className={classes.img} src={product.img} alt={product.name} />
-            <div className={classes.text}>
-              <h3>Product: {product.name}</h3>
-              <h5>Set: {product.shortDescription}</h5>
-              <h3>Price: {formatPrice(product.price)}</h3>
+            <div className={classes['buttons-container']}>
+              <BsBasket className={classes['shopping-button']} />
+              <Link to={product.slug} key={product._id} title="Details" >
+                <TbListDetails className={classes['details-button']} />
+              </Link>
             </div>
-          </Link>
+          </div>
         );
       })}
     </div>
