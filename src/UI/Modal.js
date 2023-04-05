b2605dffbab1a59cb67631a535c27986b9537639
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-04-05 09:16:55 +0100

    Create form modal overlay

---
+++ src/UI/Modal.js
@@ -0,0 +1,32 @@
+import { useNavigate } from 'react-router-dom';
+
+import classes from './Modal.module.css';
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClose} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const Modal = (props) => {
+  const navigate = useNavigate();
+
+  const closeHandler = () => {
+    return navigate(-1);
+  };
+
+  return (
+    <>
+      <Backdrop onClose={closeHandler} />
+      <ModalOverlay> {props.children} </ModalOverlay>
+    </>
+  );
+};
+
+export default Modal;
