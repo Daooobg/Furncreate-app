db38be0121cd390a0542c201d56c951d258f50e2
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-04-11 13:55:47 +0100

    Create navigation button hook

--- src/hooks/useButtons.js
+++ src/hooks/useButtons.js
@@ -7,3 +7,16 @@ export const ShoppingButtons = (props) => {
     </button>
   );
 };
+
+export const NavigationButtons = (props) => {
+  let currentClass = `${classes['navigation-button']}`;
+  if (props.category) {
+    currentClass += ` ${classes.active}`;
+  }
+
+  return (
+    <button className={currentClass} onClick={props.action} name={props.name}>
+      {props.content}
+    </button>
+  );
+};
