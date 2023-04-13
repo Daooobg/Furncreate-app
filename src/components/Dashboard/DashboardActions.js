e61d0dac387dab86b2ae7ff69c2874b81401d523
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-04-13 19:40:09 +0100

    Dashboard files changes

--- src/components/Dashboard/DashboardActions.js
+++ src/components/Dashboard/DashboardActions.js
@@ -1,53 +1,15 @@
-import { useState } from 'react';
-import { NavigationButtons } from '../../hooks/useButtons';
-import classes from './DashboardActions.module.css';
-import DashboardData from './DashboardData';
+import DashboardAdmin from './DashboardAdmin';
 
-const DashboardActions = () => {
-  const [text, setText] = useState(true);
-  const [action, setAction] = useState();
-
-  const actionHandler = (e) => {
-    setAction({ products: false, users: false });
-    setText(false);
-    if (e.target.name === 'products') {
-      setAction('products');
-    } else if (e.target.name === 'users') {
-      setAction('users');
+const DashboardActions = (props) => {
+  const Action = () => {
+    if (props.userData.role === 'admin') {
+      return <DashboardAdmin />;
+    } else {
+      return <h1>Something went wrong!</h1>;
     }
   };
 
-  return (
-    <>
-      <div className={classes['buttons-container']}>
-        <NavigationButtons
-          name="products"
-          content="products"
-          action={actionHandler}
-        />
-        <NavigationButtons
-          name="users"
-          content="users"
-          action={actionHandler}
-        />
-      </div>
-      {text && (
-        <div>
-          <h2 className={classes['button-introduction']}>
-            {`<---You are admin and you can check create edit and delete products when you
-            pres Product button`}
-          </h2>
-
-          <h2 className={classes['button-introduction']}>
-            {`<---You are admin and you can see all users and change  users role when you
-            pres Users button`}
-          </h2>
-        </div>
-      )}
-      {!text && <DashboardData action={action} /> }
-
-    </>
-  );
+  return <Action />;
 };
 
 export default DashboardActions;
