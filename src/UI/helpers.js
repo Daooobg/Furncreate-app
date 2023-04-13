93f8a559c91b9ea176d7fe39666e1adaff3e43bf
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-04-13 15:29:33 +0100

    create form field helper

--- src/UI/helpers.js
+++ src/UI/helpers.js
@@ -9,3 +9,18 @@ export const getUniqueValues = (data, type) => {
   let unique = data.map((item) => item[type]);
   return ['all', ...new Set(unique)];
 };
+
+export const formField = (name, type, data, action, disabled) => {
+  return (
+    <>
+      <label htmlFor={name}>{name}</label>
+      <input
+        value={data}
+        type={type}
+        name={name}
+        onChange={action}
+        disabled={disabled}
+      />
+    </>
+  );
+};
