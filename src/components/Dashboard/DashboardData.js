2376f25852834bbb7a911995ab8221848607f357
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-04-14 13:20:16 +0100

    add new actions

--- src/components/Dashboard/DashboardData.js
+++ src/components/Dashboard/DashboardData.js
@@ -1,13 +1,39 @@
+import ActionComments from './ActionComments';
 import ActionProducts from './ActionProducts';
+import ActionProfile from './ActionProfile';
+import ActionPurchases from './ActionPurchases';
 import ActionUsers from './ActionUsers';
 import classes from './DashboardData.module.css';
 
-
 const DashboardData = (props) => {
+
   return (
     <>
-      {props.action === 'products' && <div className={classes["product-container"]}><ActionProducts /> </div>}
-      {props.action === 'users' && <div className={classes["user-container"]}><ActionUsers /> </div>}
+      {props.action === 'products' && (
+        <div className={classes['product-container']}>
+          <ActionProducts />
+        </div>
+      )}
+      {props.action === 'users' && (
+        <div className={classes['user-container']}>
+          <ActionUsers />
+        </div>
+      )}
+      {props.action === 'profile' && (
+        <div>
+          <ActionProfile userData={props.userData} />
+        </div>
+      )}
+      {props.action === 'comments' && (
+        <div>
+          <ActionComments />
+        </div>
+      )}
+      {props.action === 'purchases' && (
+        <div>
+          <ActionPurchases />
+        </div>
+      )}
     </>
   );
 };
